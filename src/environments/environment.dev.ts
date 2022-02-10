8d9fdd7bafb2e76ea0138ac8a4785bc83224b0cd
mikeJT <jeerasaktarat@gmail.com>
2022-02-10 10:39:13 +0700

    remove-src-duplicated

--- src/environments/environment.dev.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  graphqlUri: 'http://13.250.41.39:8082/graphql'
-};
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  graphqlUri: 'http://52.74.162.217:8085/graphql'
-
-};
